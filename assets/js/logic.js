22f243f8149179a73d85340f9d681122f04cd65a
lydiabarham <lcbarham9@gmail.com>
2023-12-04 17:16:13 +0000

    set timer interval function to count down from 60

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -3,6 +3,7 @@ const startButton = document.querySelector("#start");
 const questionSection = document.querySelector("#questions");
 const startScreen = document.querySelector("#start-screen");
 const endScreen = document.querySelector("#end-screen");
+const timer = document.querySelector("#time");
 
 let currentQuestionIndex = 0;
 
@@ -33,13 +34,16 @@ const showQuestion = function (index) {
     questionSection.appendChild(questionContainer);
 };
 
+// create function to start quiz
 const startQuiz = function (event) {
     event.preventDefault();
+    setTime();
     questionSection.setAttribute("class", "show");
     showQuestion(currentQuestionIndex);
     startScreen.setAttribute("class", "hide");
 }
 
+// create funtion to move to next question
 const nextQuestion = function () {
     currentQuestionIndex++;
     if (currentQuestionIndex < questionsArray.length) {
@@ -49,7 +53,25 @@ const nextQuestion = function () {
     }
 };
 
+// add event listeners
 startButton.addEventListener("click", startQuiz);
 questionSection.addEventListener("click", nextQuestion);
 
+// set countdown timer
+let secondsLeft = "60";
+
+const setTime = function() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timer.textContent = secondsLeft;
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      endScreen.setAttribute("class", "show");
+    }
+
+  }, 1000);
+}
 
